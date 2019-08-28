5f67516689e69b6dfccfd884b76b81c1bffad10c
liut <liut@liut.xyz>
2019-08-28 18:35:44 +0800

    360 加固相关

--- buildSrc/src/main/groovy/xyz/liut/releaseplugin/ReleasePlugin.groovy
+++ buildSrc/src/main/groovy/xyz/liut/releaseplugin/ReleasePlugin.groovy
@@ -8,9 +8,11 @@ import com.android.build.gradle.internal.dsl.ProductFlavor
 import com.android.utils.StringHelper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import xyz.liut.logcat.L
 import xyz.liut.logcat.Logcat
 import xyz.liut.logcat.handler.StdHandler
+import xyz.liut.releaseplugin.task.BaseTask
 import xyz.liut.releaseplugin.task.JiaguTask
 import xyz.liut.releaseplugin.task.ReleaseTask
 
@@ -132,6 +134,80 @@ class ReleasePlugin implements Plugin<Project> {
                 })
             }
         })
+
+        create360Task()
+    }
+
+    /**
+     * 360 加固 相关
+     */
+    def create360Task() {
+
+        // 加固程序登录
+        Task task = project.task("login360Jiagu", type: BaseTask, group: '360Jiagu', description: "360Jiagu login") {
+            doLast {
+                String jiaguProgramDir = localPropertiesMap.get("jiaguPath")
+                String account = localPropertiesMap.get("account360")
+                String passwd = localPropertiesMap.get("passwd360")
+
+                if (!jiaguProgramDir) {
+                    throw new IllegalArgumentException("jiaguPath 为空, 请在项目根目录的 local.properties 中配置 jiaguPath")
+                }
+                if (!account) {
+                    throw new IllegalArgumentException("account360 为空, 请在项目根目录的 local.properties 中配置 account360")
+                }
+                if (!passwd) {
+                    throw new IllegalArgumentException("passwd360 为空, 请在项目根目录的 local.properties 中配置 passwd360")
+                }
+
+                // 登录账户
+                Utils.execCommand("java -jar $jiaguProgramDir -login ${account} ${passwd}")
+            }
+
+        }
+
+        // 加固初始化
+        project.task("init360Jiagu", type: BaseTask, dependsOn: task, group: '360Jiagu', description: "360Jiagu init") {
+            doLast {
+                String jiaguProgramDir = localPropertiesMap.get("jiaguPath")
+                String apkSigning = localPropertiesMap.get("apkSigning")
+                if (!jiaguProgramDir) {
+                    throw new IllegalArgumentException("jiaguPath 为空, 请在项目根目录的 local.properties 中配置 jiaguPath")
+                }
+                if (!apkSigning) {
+                    throw new IllegalArgumentException("apkSigning 为空, 请在项目根目录的 local.properties 中配置 apkSigning")
+                }
+
+                def signing = android.signingConfigs.find { it.name == apkSigning }
+
+                def jiaguPath = jiaguProgramDir
+                def storeFile = signing.storeFile
+                def keyAlias = signing.keyAlias
+                def keyPassword = signing.keyPassword
+                def storePassword = signing.storePassword
+
+                // 导入证书 <keystore_path><keystore_password><alias><alias_password>
+                Utils.execCommand("java -jar $jiaguPath -importsign ${storeFile} ${storePassword} ${keyAlias} ${keyPassword}")
+                // 清空增值服务
+                Utils.execCommand("java -jar $jiaguPath -config ''")
+            }
+
+        }
+
+        // 启动 gui 程序
+        project.task("launch360Jiagu", type: BaseTask, group: '360Jiagu', description: "launch 360Jiagu") {
+            doLast {
+
+                String jiaguProgramDir = localPropertiesMap.get("jiaguPath")
+                if (!jiaguProgramDir) {
+                    throw new IllegalArgumentException("jiaguPath 为空, 请在项目根目录的 local.properties 中配置 jiaguPath")
+                }
+
+                Utils.execCommand("java -jar $jiaguProgramDir")
+            }
+
+        }
+
     }
 
     /**
